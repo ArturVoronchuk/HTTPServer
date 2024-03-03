a10e659e2e42af966da5e99014caf357295d24b9
artur.voronchuk <artur.voronchuk@rt.ru>
2024-03-03 22:14:02 +0300

    HomeTask 1.2v2 added

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -3,11 +3,19 @@ import org.apache.commons.collections.map.MultiValueMap;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 public class Request {
+    private final String method;
+    private final String path;
+    private final List<String> headers;
+
     public static MultiMap getQueryParams(String url) {
         MultiMap parameter = new MultiValueMap();
         List<NameValuePair> params;
@@ -23,6 +31,23 @@ public class Request {
         return parameter;
     }
 
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private List<NameValuePair> params;
+
+    public Request(String method, String path) {
+        this.method = method;
+        this.path = path;
+        headers = null;
+    }
+
+    public Request(String method, String path, List<String> headers, List<NameValuePair> params) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.params = params;
+    }
+
     public static String getQueryParamsPath(String url) {
         String result;
         int i = url.indexOf("?");
@@ -31,5 +56,61 @@ public class Request {
         }
         result = url.substring(0, i);
         return result;
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    static Request createRequest(BufferedInputStream in) throws IOException, URISyntaxException {
+        final List<String> allowedMethods = List.of(GET, POST);
+        final var limit = 4096;
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            return null;
+        }
+
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            return null;
+        }
+
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            return null;
+        }
+
+        final var path = requestLine[1];
+
+        final var headerDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headerDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            return null;
+        }
+
+        in.reset();
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        List<String> headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+        List<NameValuePair> params = URLEncodedUtils.parse(new URI(path), StandardCharsets.UTF_8);
+
+        return new Request(method, path, headers, params);
     }
 }
